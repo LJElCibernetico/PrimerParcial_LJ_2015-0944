04cd899fea804f10ff263007db2f2258db9b35ee
Luis Jose Enrique Ramos Mercedes <luis_ramos1@ucne.edu.do>
2018-09-25 22:12:54 -0400

    Creando el proyecto y haciendo las entidades

---
+++ Entidades/CuentasBancarias.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class CuentasBancarias
+    {
+        public int CuentaId { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Nombre { get; set; }
+        public double Balance { get; set; }
+
+        public CuentasBancarias(int cuentaId, DateTime fecha, string nombre, double balance)
+        {
+            CuentaId = cuentaId;
+            Fecha = fecha;
+            Nombre = nombre;
+            Balance = balance;
+        }
+
+        public CuentasBancarias()
+        {
+            CuentaId = 0;
+            Fecha = DateTime.Now;
+            Nombre = String.Empty;
+            Balance = 0;
+        }
+    }
+}
---
+++ Entidades/Depositos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Depositos
+    {
+        public int DepositosId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int CuentaId { get; set; }
+        public string Concepto { get; set; }
+        public double Monto { get; set; }
+
+        public Depositos(int depositosId, DateTime fecha, int cuentaId, string concepto, double monto)
+        {
+            DepositosId = depositosId;
+            Fecha = fecha;
+            CuentaId = cuentaId;
+            Concepto = concepto;
+            Monto = monto;
+        }
+
+        public Depositos()
+        {
+            DepositosId = 0;
+            Fecha = DateTime.Now;
+            CuentaId = 0;
+            Concepto = String.Empty;
+            Monto = 0;
+        }
+    }
+}
